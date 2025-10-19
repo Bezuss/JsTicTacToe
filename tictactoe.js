e2d62998d0cd73fd9b074f72b7a417a18b274c40
Bezus <102990729+Bezuss@users.noreply.github.com>
2025-10-19 21:53:21 +0200

    Prettier edited

--- tictactoe.js
+++ tictactoe.js
@@ -1,51 +1,77 @@
 // Practice #1
 
 class Game {
+  constructor() {
+    this.try = 0;
+    this.mas = {};
+  }
 
-    constructor() {
-        this.try = 0;
-        this.mas = {};
+  click(x, y) {
+    if (this.try % 2 != 0) {
+      if (this.mas["A" + x + "" + y] == null) this.mas["A" + x + "" + y] = 1;
+      else {
+        console.log("Клеточка уже занята");
+        return;
+      }
+    } else if (this.mas["A" + x + "" + y] == null) {
+      this.mas["A" + x + "" + y] = 0;
+    } else {
+      console.log("Клеточка уже занята");
+      return;
     }
 
-    click(x, y) {
-
-        if(this.try%2!=0){
-            if(this.mas["A" + x + "" + y] == null)
-                this.mas["A" + x + "" + y] = 1;
-            else{
-                console.log("Клеточка уже занята")
-                return;
-            }
-        }
-        else
-            if(this.mas["A" + x + "" + y] == null){
-                this.mas["A" + x + "" + y] = 0;
-            }
-            else{
-                console.log("Клеточка уже занята")
-                return;  
-            }
-        
-            // Проверка победителя
-        this.try++;
-         if (this.mas["A00"] === this.mas["A01"] && this.mas["A00"] === this.mas["A02"] && this.mas["A00"] !== undefined) {
-        console.log(`Игрок ${this.mas["A00"] + 1} выиграл!`);
-    } else if (this.mas["A10"] === this.mas["A11"] && this.mas["A10"] === this.mas["A12"] && this.mas["A10"] !== undefined) {
-        console.log(`Игрок ${this.mas["A10"] + 1} выиграл!`);
-    } else if (this.mas["A20"] === this.mas["A21"] && this.mas["A20"] === this.mas["A22"] && this.mas["A20"] !== undefined) {
-        console.log(`Игрок ${this.mas["A20"] + 1} выиграл!`);
-    } else if (this.mas["A00"] === this.mas["A10"] && this.mas["A00"] === this.mas["A20"] && this.mas["A00"] !== undefined) {
-        console.log(`Игрок ${this.mas["A00"] + 1} выиграл!`);
-    } else if (this.mas["A01"] === this.mas["A11"] && this.mas["A01"] === this.mas["A21"] && this.mas["A01"] !== undefined) {
-        console.log(`Игрок ${this.mas["A01"] + 1} выиграл!`);
-    } else if (this.mas["A02"] === this.mas["A12"] && this.mas["A02"] === this.mas["A22"] && this.mas["A02"] !== undefined) {
-        console.log(`Игрок ${this.mas["A02"] + 1} выиграл!`);
-    } else if (this.mas["A00"] === this.mas["A11"] && this.mas["A00"] === this.mas["A22"] && this.mas["A00"] !== undefined) {
-        console.log(`Игрок ${this.mas["A00"] + 1} выиграл!`);
-    } else if (this.mas["A02"] === this.mas["A11"] && this.mas["A02"] === this.mas["A20"] && this.mas["A02"] !== undefined) {
-        console.log(`Игрок ${this.mas["A02"] + 1} выиграл!`);
+    // Проверка победителя
+    this.try++;
+    if (
+      this.mas["A00"] === this.mas["A01"] &&
+      this.mas["A00"] === this.mas["A02"] &&
+      this.mas["A00"] !== undefined
+    ) {
+      console.log(`Игрок ${this.mas["A00"] + 1} выиграл!`);
+    } else if (
+      this.mas["A10"] === this.mas["A11"] &&
+      this.mas["A10"] === this.mas["A12"] &&
+      this.mas["A10"] !== undefined
+    ) {
+      console.log(`Игрок ${this.mas["A10"] + 1} выиграл!`);
+    } else if (
+      this.mas["A20"] === this.mas["A21"] &&
+      this.mas["A20"] === this.mas["A22"] &&
+      this.mas["A20"] !== undefined
+    ) {
+      console.log(`Игрок ${this.mas["A20"] + 1} выиграл!`);
+    } else if (
+      this.mas["A00"] === this.mas["A10"] &&
+      this.mas["A00"] === this.mas["A20"] &&
+      this.mas["A00"] !== undefined
+    ) {
+      console.log(`Игрок ${this.mas["A00"] + 1} выиграл!`);
+    } else if (
+      this.mas["A01"] === this.mas["A11"] &&
+      this.mas["A01"] === this.mas["A21"] &&
+      this.mas["A01"] !== undefined
+    ) {
+      console.log(`Игрок ${this.mas["A01"] + 1} выиграл!`);
+    } else if (
+      this.mas["A02"] === this.mas["A12"] &&
+      this.mas["A02"] === this.mas["A22"] &&
+      this.mas["A02"] !== undefined
+    ) {
+      console.log(`Игрок ${this.mas["A02"] + 1} выиграл!`);
+    } else if (
+      this.mas["A00"] === this.mas["A11"] &&
+      this.mas["A00"] === this.mas["A22"] &&
+      this.mas["A00"] !== undefined
+    ) {
+      console.log(`Игрок ${this.mas["A00"] + 1} выиграл!`);
+    } else if (
+      this.mas["A02"] === this.mas["A11"] &&
+      this.mas["A02"] === this.mas["A20"] &&
+      this.mas["A02"] !== undefined
+    ) {
+      console.log(`Игрок ${this.mas["A02"] + 1} выиграл!`);
     }
-    }
+  }
 }
 
 const game = new Game();
